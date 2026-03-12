0e861a7516d2691824f507734eaec281f7b6a0bc
Muhammad Ali <softeng.aliijaz@gmail.com>
2026-03-13 01:19:51 +0500

    Blogify - Front-End (Static-READY) &  STRAPI Setup Configured (READy to integrate api)

--- blogify-client/app/page.tsx
+++ blogify-client/app/page.tsx
@@ -1,6 +1,22 @@
 import Blogs from "./components/Blogs";
 import Categories from "./components/Categories";
 
+async function fetchCategories() {
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    },
+  };
+
+  const res = await fetch(
+    `${process.env.STRAPI_API_TOKEN}/api/categories`,
+    options,
+  );
+  const data = await res.json();
+  return data;
+}
+
 export default function Home() {
   return (
     <div className="space-y-10">
